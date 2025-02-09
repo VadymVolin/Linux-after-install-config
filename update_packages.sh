c92b72fa510e7de72688401994e50089903ab300
Vadym Volin <39218811+VadymVolin@users.noreply.github.com>
2025-02-09 11:09:31 +0200

    Update update_packages.sh
    added comments

--- update_packages.sh
+++ update_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# GNOME
 # # gnome-terminal -- bash --noclose -e 'echo -e "Hello, $USER Start updating with command [sudo apt-get update && sudo apt-get upgrade -y]...";
 # sudo apt-get update && sudo apt-get upgrade -y;
 # # wait for any user key
@@ -12,6 +12,7 @@
 #
 # '
 
+# Konsole
 # Open konsole terminal
 konsole --noclose -e $SHELL -c 'echo -e "Hello, $USER \nStart updating with command [sudo apt-get update && sudo apt-get upgrade -y]...";
 sudo apt-get update && sudo apt-get upgrade -y;
