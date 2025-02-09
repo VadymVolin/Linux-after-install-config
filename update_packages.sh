e0dbc36e3b9e3eb5876bae29ffcfb3f97ec76580
Vadym Volin <vadim.volin1@gmail.com>
2025-02-09 11:04:58 +0200

    removed unused file, added script to update packages

---
+++ update_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# # gnome-terminal -- bash --noclose -e 'echo -e "Hello, $USER Start updating with command [sudo apt-get update && sudo apt-get upgrade -y]...";
+# sudo apt-get update && sudo apt-get upgrade -y;
+# # wait for any user key
+# echo -e "\nThank you, $USER. \nPress [Enter] key to continue...";
+# read -p "";
+#
+# # new cmd session
+# clear;
+# $SHELL;
+#
+# '
+
+# Open konsole terminal
+konsole --noclose -e $SHELL -c 'echo -e "Hello, $USER \nStart updating with command [sudo apt-get update && sudo apt-get upgrade -y]...";
+sudo apt-get update && sudo apt-get upgrade -y;
+# wait for any user key
+echo -e "\nThank you, $USER. \nPress [Enter] key to continue...";
+read -p "";
+
+# new cmd session
+clear;
+$SHELL;
+
+'
+
